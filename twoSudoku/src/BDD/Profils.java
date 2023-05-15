dda00a3d9b6cfc757524709b81bda3c5416c0912
florian thubin <florian.thubin@gmail.com>
2023-05-15 14:54:26 +0200

    ajout bouton crée et supprimez user

--- twoSudoku/src/BDD/Profils.java
+++ twoSudoku/src/BDD/Profils.java
@@ -8,13 +8,21 @@ import java.sql.ResultSet;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.swing.JOptionPane;
+import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 
 public class Profils {
     private JFrame frame;
     private JComboBox<String> comboBox;
+    private JButton createButton;
+    private JButton deleteButton;
+
 
     public static void main(String[] args) {
         EventQueue.invokeLater(new Runnable() {
@@ -44,7 +52,26 @@ public class Profils {
 
         comboBox = new JComboBox<String>();
         panel.add(comboBox);
+        
+        createButton = new JButton("Créer Utilisateur");
+        panel.add(createButton);
+        
+        deleteButton = new JButton("Supprimer Utilisateur");
+        panel.add(deleteButton);
+        
+        createButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                createUser();
+            }
+        });
+        
+        deleteButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                deleteUser();
+            }
+        });
     }
+    
 
     private void populateComboBox() {
         // Vos informations de connexion à une base de données
@@ -67,13 +94,44 @@ public class Profils {
                 int playerId = resultSet.getInt("Player_id");
                 String name = resultSet.getString("Name");
                 String profile = playerId + ": " + name;
-                profiles.add(profile);
-            }
-
-            // Ajouter les profils à la liste déroulante
-            for (String profile : profiles) {
                 comboBox.addItem(profile);
             }
+            conn.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("Erreur");
+            System.exit(0);
+        }
+    }
+
+
+            
+            private void createUser() {
+                // Vos informations de connexion à la base de données
+                String BDD = "sudoku";
+                String url = "jdbc:mysql://localhost:3306/" + BDD;
+                String user = "root";
+                String passwd = "";
+
+                try {
+                    Class.forName("com.mysql.cj.jdbc.Driver");
+                    Connection conn = DriverManager.getConnection(url, user, passwd);
+
+                    // Obtenir le nom d'utilisateur pour le nouvel utilisateur (par exemple, à partir d'un champ de texte)
+                    String username = JOptionPane.showInputDialog(frame, "Nom d'utilisateur :");
+
+                    // Insérer le nouvel utilisateur dans la base de données
+                    String query = "INSERT INTO player (Name) VALUES (?)";
+                    PreparedStatement statement = conn.prepareStatement(query);
+                    statement.setString(1, username);
+                    int rowsInserted = statement.executeUpdate();
+
+                    if (rowsInserted > 0) {
+                        JOptionPane.showMessageDialog(frame, "Utilisateur créé avec succès !");
+                    } else {
+                        JOptionPane.showMessageDialog(frame, "Erreur lors de la création de l'utilisateur.");
+                    }
+             
 
             conn.close();
         } catch (Exception e) {
@@ -82,4 +140,41 @@ public class Profils {
             System.exit(0);
         }
     }
+
+
+private void deleteUser() {
+    // Vos informations de connexion à la base de données
+    String BDD = "sudoku";
+    String url = "jdbc:mysql://localhost:3306/" + BDD;
+    String user = "root";
+    String passwd = "";
+
+    try {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        Connection conn = DriverManager.getConnection(url, user, passwd);
+
+        // Obtenir l'utilisateur sélectionné dans la liste déroulante
+        String selectedProfile = comboBox.getSelectedItem().toString();
+        int playerId = Integer.parseInt(selectedProfile.split(":")[0].trim());
+
+        // Supprimer l'utilisateur de la base de données
+        String query = "DELETE FROM player WHERE Player_id = ?";
+        PreparedStatement statement = conn.prepareStatement(query);
+        statement.setInt(1, playerId);
+        int rowsDeleted = statement.executeUpdate();
+
+        if (rowsDeleted > 0) {
+            JOptionPane.showMessageDialog(frame, "Utilisateur supprimé avec succès !");
+            populateComboBox();
+        } else {
+            JOptionPane.showMessageDialog(frame, "Erreur lors de la suppression de l'utilisateur.");
+        }
+
+        conn.close();
+    } catch (Exception e) {
+        e.printStackTrace();
+        System.out.println("Erreur");
+        System.exit(0);
+    }
+}
 }
